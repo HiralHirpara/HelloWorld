51b7a04221b8621c179d96b776225676c85b50a7
HiralHirpara <hirparahiral01@gmail.com>
2024-02-01 18:08:02 -0800

    How to iterate values using loops in java

---
+++ HelloWorld/src/BeginnerJava/LoopInJava.java
@@ -0,0 +1,22 @@
+package BeginnerJava;
+
+public class LoopInJava {
+
+	public static void main(String[] args) {
+		
+		// How to iterate number from 1 to 10 in java using "FOR" loop
+		System.out.println(":::Print FOR loop data:::");
+		for(int f = 1;f<=10;f++) {
+			System.out.println(f);
+		}
+		
+		// How to iterate number from 1 to 10 in java using "WHILE" loop
+		System.out.println(":::Print WHILE loop data:::");
+		int w = 1;
+		while(w <= 10) {
+			System.out.println(w);
+			w++;
+		}
+	}
+
+}
