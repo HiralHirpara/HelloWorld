2e2e6cceceea28283140db4f3c911002d859665b
HiralHirpara <hirparahiral01@gmail.com>
2024-02-13 21:36:28 -0800

    added if else condition

---
+++ HelloWorld/src/BeginnerJava/IFElseInJava.java
@@ -0,0 +1,25 @@
+package BeginnerJava;
+
+public class IFElseInJava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i = 10;
+		
+		if(i == 10) {
+			System.out.println(i + " :: Value is matched!! ");
+		}else {
+			System.out.println(i+" :: Value is not matched!! ");
+		}
+		
+		i = 5;
+		if(i == 10) {
+			System.out.println(i + " :: Value is matched!! ");
+		}else {
+			System.out.println(i+" :: Value is not matched!! ");
+		}
+		
+
+	}
+
+}
