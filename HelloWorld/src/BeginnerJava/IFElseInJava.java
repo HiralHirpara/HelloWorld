480eb53fc876b003ae03ab36edf5bc511c66bfea
HiralHirpara <hirparahiral01@gmail.com>
2024-02-13 21:45:02 -0800

    added  nested if else

--- HelloWorld/src/BeginnerJava/IFElseInJava.java
+++ HelloWorld/src/BeginnerJava/IFElseInJava.java
@@ -3,7 +3,7 @@ package BeginnerJava;
 public class IFElseInJava {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// If else statement
 		int i = 10;
 		
 		if(i == 10) {
@@ -17,6 +17,24 @@ public class IFElseInJava {
 			System.out.println(i + " :: Value is matched!! ");
 		}else {
 			System.out.println(i+" :: Value is not matched!! ");
+		}
+		
+		// Nested if else statement
+		
+		int j = 1;
+		
+		if(i == 5) {
+			if(j == 1) {
+				System.out.println(j + " :: Nested value is matched!! ");
+			}else {
+				System.out.println(j +" :: Nested value is not matched!! ");
+			}
+		}else {
+			if(j == 5) {
+				System.out.println(j + " :: Nested value is matched!! ");
+			}else {
+				System.out.println(j +" :: Nested value is not matched!! ");
+			}
 		}
 		
 
